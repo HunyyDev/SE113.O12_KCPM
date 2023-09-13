ebe0bec5437ce7a7ddd0145fc7340d941b7ca3a9
HungNP <21520252@gm.uit.edu.vn>
2023-09-13 18:28:47 +0700

    test

--- server/server.js
+++ server/server.js
@@ -4,19 +4,9 @@ const compression = require("compression");
 const zmq = require("zeromq");
 const path = require("path");
 const fs = require("fs");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const storage = multer.diskStorage({
-    destination: (_req, _file, cb) => {
-        cb(null, "/app/server/uploads/");
-    },
-    filename: (_req, _file, cb) => {
-        const uniqueSuffix = Date.now().toString();
-        cb(null, uniqueSuffix);
-    },
-});
-
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.diskStorage({}) });
 
 const PORT = process.env.PORT || 7860;
 const app = express();
@@ -39,46 +29,54 @@ function init() {
 async function main() {
     init();
     app.use("/static", express.static("static"));
-    app.post(
-        "/upload_image",
-        upload.single("image"),
-        async (req, res) => {
-            if (!req.file.path) {
-                console.error("no file received")
-                return;
-            }
-            const resultPath = await handleInferenceImage(
-                req.file.path,
-                req.body.threshold
-            );
 
-            const readStream = fs.createReadStream(resultPath);
+    // const uploadImage = upload.single("image");
 
-            readStream.on("open", () => {
-                res.setHeader(
-                    "Content-Disposition",
-                    "attachment; filename=image.png"
-                );
-                res.setHeader("Content-Type", "image/png");
-                readStream.pipe(res);
-            });
+    // app.post('/upload_image', function (req, res) {
+    //     uploadImage(req, res, function (err) {
+    //         console.error(err)
+    //     });
+    // });
 
-            readStream.on("error", (err) => {
-                res.status(500).json({
-                    message: err,
-                });
-            });
+    app.post("/upload_image", upload.single("image"), (req, res) => {
+        console.log(req.file)
+        res.sendStatus(200);
+    })
 
-            readStream.on("close", () => {
-                fs.unlink(resultPath, (err) => {
-                    if (err) console.error(err);
-                });
-                fs.unlink(req.file.path, (err) => {
-                    if (err) console.error(err);
-                });
-            });
-        }
-    );
+    // console.log(req, res, error)
+    // if (error) console.error(error)
+
+    // const resultPath = await handleInferenceImage(
+    //     req.file.path,
+    //     req.body.threshold
+    // );
+
+    // const readStream = fs.createReadStream(resultPath);
+
+    // readStream.on("open", () => {
+    //     res.setHeader(
+    //         "Content-Disposition",
+    //         "attachment; filename=image.png"
+    //     );
+    //     res.setHeader("Content-Type", "image/png");
+    //     readStream.pipe(res);
+    // });
+
+    // readStream.on("error", (err) => {
+    //     res.status(500).json({
+    //         message: err,
+    //     });
+    // });
+
+    // readStream.on("close", () => {
+    //     fs.unlink(resultPath, (err) => {
+    //         if (err) console.error(err);
+    //     });
+    //     fs.unlink(req.file.path, (err) => {
+    //         if (err) console.error(err);
+    //     });
+    // });
+    // });
 
     app.post(
         "/upload_video",
