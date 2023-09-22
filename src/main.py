521e539f2b8f522edcba6fab7c0b05e6b9f22a2e
HungNP <21520252@gm.uit.edu.vn>
2023-09-22 17:04:00 +0700

    remove print

--- src/main.py
+++ src/main.py
@@ -186,7 +186,6 @@ model_path = "./model"
 detector = Detector(model_path=model_path, device_name="cpu", device_id=0)
 
 url: str = os.environ.get("SUPABASE_URL")
-print(url)
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
