33c270ea8430a6ec194e92dd6a274eacdeb7b4a3
NMPhap <21522441@gm.uit.edu.vn>
2023-11-06 09:08:50 +0700

    <dev>: change CI/CD

--- app/main.py
+++ app/main.py
@@ -1,26 +1,26 @@
 from typing_extensions import Annotated
 from fastapi import Depends, FastAPI
-
 from app.dependencies import get_current_user
 from .routers import image, video
 
+def createApp():
+    app = FastAPI()
+    app.include_router(image.router)
+    app.include_router(video.router)
 
-app = FastAPI()
-
-app.include_router(image.router)
-app.include_router(video.router)
-
-
-@app.get("/me")
-def getProfile(current_user: Annotated[any, Depends(get_current_user)]):
-    return current_user
+    @app.get("/me")
+    def getProfile(current_user: Annotated[any, Depends(get_current_user)]):
+        return current_user
 
 
-@app.post("/login")
-def login(email: str, password: str):
-    return "not implemented yet"
+    @app.post("/login")
+    def login(email: str, password: str):
+        return "not implemented yet"
 
 
-@app.get("/")
-def hello():
-    return "Hello World!"
+    @app.get("/")
+    def hello():
+        return "Hello World!"
+    return app
+    
+createApp()
--- app/routers/test_image.py
+++ app/routers/test_image.py
@@ -6,24 +6,52 @@ from app.routers.image import handleImageRequest
 import requests
 import pytest
 from io import StringIO
-
-class TestImageRoute:
-    img = mmcv.imread("demo.jpg")
-    url = "http://0.0.0.0:3000"
+import pytest
+from app.main import createApp
+from PIL import Image
+from fastapi.testclient import TestClient
+@pytest.fixture
+def client():
+    client = TestClient(createApp(), "http://0.0.0.0:3000")
+    yield client
+class TestImageRoute():
+    img = mmcv.imread('demo.jpg')
+    url = "http://0.0.0.0:3000/image"
     def test_inferenceImage(self):
         bboxes, labels = inferenceImage(self.img, 0.3, True)
         assert len(bboxes.tolist()) > 0 and len(labels.tolist()) > 0 and len(bboxes.tolist()) == len(labels.tolist())
         result = inferenceImage(self.img, 0.3)
         assert type(result) is np.ndarray and result.shape == self.img.shape
-    def test_ImageAPI(self):
-        with pytest.raises(NotImplementedError):
-            payload = {}
-            files=[
-            ('file',('demo.jpg',open('demo.jpg','rb'),'image/jpeg'))
-            ]
-            headers = {
-            'accept': 'application/json'
-            }
-            response = requests.request("POST", self.url, headers=headers, data=payload, files=files)
-            assert response.status_code == 200
+    def test_ImageAPI(self, client):
+        payload = {}
+        files=[
+        ('file',('demo.jpg',open('demo.jpg','rb'),'image/jpeg'))
+        ]
+        headers = {
+        'accept': 'application/json'
+        }
+        response = client.request("POST", "image", headers=headers, data=payload, files=files)
+        result = mmcv.imfrombytes(response.read())
+        assert response.status_code == 200 and result.shape == self.img.shape
+    # def test_ImageAPI_one_channel_array(self, client):
+    #     np.zeros((1,640,640)).dump("one_channel.jpg")
+    #     payload = {}
+    #     files=[
+    #         ('file', ("demo.jpg",open("one_channel.jpg", "rb"),'image/jpeg'))
+    #     ]
+    #     headers = {
+    #     'accept': 'application/json'
+    #     }
+    #     response = client.request("POST", "image", headers=headers, data=payload, files=files)
+    #     assert response.status_code != 200
+    def test_ImageAPIWithThresHold(self, client):
+        payload = {}
+        files=[
+        ('file',('demo.jpg',open('demo.jpg','rb'),'image/jpeg'))
+        ]
+        headers = {
+        'accept': 'application/json'
+        }
+        response = client.request("POST", "image?threshold=1&raw=True", headers=headers, data=payload, files=files)
+        assert response.status_code == 200 and len(response.json()["labels"]) == 0
 
