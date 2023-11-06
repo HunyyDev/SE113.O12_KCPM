dd73539229b6a9a47d332ec9dd6ca74f89f7fcaf
HungNP <21520252@gm.uit.edu.vn>
2023-11-06 11:19:09 +0700

    redirect on GET /

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing_extensions import Annotated
 from fastapi import Depends, FastAPI
+from fastapi.responses import RedirectResponse
 
 from app.dependencies import get_current_user
 from .routers import image, video
@@ -11,16 +12,12 @@ app.include_router(image.router)
 app.include_router(video.router)
 
 
-@app.get("/me")
+@app.get("/login")
 def getProfile(current_user: Annotated[any, Depends(get_current_user)]):
     return current_user
 
 
-@app.post("/login")
-def login(email: str, password: str):
-    return "not implemented yet"
-
-
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def hello():
-    return "Hello World!"
+    response = RedirectResponse(url="/docs")
+    return response
