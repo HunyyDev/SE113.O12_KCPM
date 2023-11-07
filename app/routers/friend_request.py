299424ab709018d7681d095390ccad1968b7f415
HungNP <21520252@gm.uit.edu.vn>
2023-11-07 14:20:42 +0700

    fix qr

--- app/routers/friend_request.py
+++ app/routers/friend_request.py
@@ -1,9 +1,11 @@
 import datetime
+import io
 import cv2
+from fastapi.responses import StreamingResponse
 import qrcode
 from fastapi import APIRouter, Depends, HTTPException, Response
 from app.dependencies import get_current_user
-from app import db
+from app import db, logger
 from enum import Enum
 from app.graphdb.main import insert2PersonAndSetFriend
 
@@ -23,41 +25,42 @@ EXPIRE_MINUTES = 15
 
 @router.get("")
 def getFriendRequest(user=Depends(get_current_user)):
-    if user.uid is None:
+    if user["sub"] is None:
         raise HTTPException(status_code=400, detail="User not found")
     friend_requests = (
-        db.collection(COLLECTION_NAME).where("inviter", "==", user.uid).stream()
+        db.collection(COLLECTION_NAME).where("inviter", "==", user["sub"]).stream()
     )
     return {"friend_requests": [Request.to_dict() for Request in friend_requests]}
 
 
 @router.post("")
 def createRequest(user=Depends(get_current_user)):
-    if user.uid is None:
+    if user["sub"] is None:
         raise HTTPException(status_code=400, detail="User not found")
     _, fr_ref = db.collection(COLLECTION_NAME).add(
         {
-            "invitor": user.uid,
+            "invitor": user["sub"],
             "status": RequestStatus.WAITING_INVITEE.value,
-            "expire_at": datetime.now() + datetime.timedelta(minutes=EXPIRE_MINUTES),
+            "expire_at": datetime.datetime.now()
+            + datetime.timedelta(minutes=EXPIRE_MINUTES),
         }
     )
-    qr = qrcode.make(fr_ref.id)
 
-    ret, png = cv2.imencode(".jpg", qr)
-
-    if not ret:
+    try:
+        qr = qrcode.make(fr_ref.id)
+        buf = io.BytesIO()
+        qr.save(buf)
+        buf.seek(0)
+        return StreamingResponse(buf, media_type="image/png")
+    except Exception as e:
+        logger.error(e)
         fr_ref.delete()
         return Response(content="Failed to encode image", status_code=500)
-
-    png_bytes = png.tobytes()
-
-    return Response(content=png_bytes, media_type="image/png")
 
 
 @router.patch("/{RequestId}")
 def acceptRequest(RequestId: str, user=Depends(get_current_user)):
-    if user.uid is None:
+    if user["sub"] is None:
         raise HTTPException(status_code=400, detail="User not found")
 
     fr_ref = db.collection(COLLECTION_NAME).document(RequestId)
@@ -79,7 +82,7 @@ def acceptRequest(RequestId: str, user=Depends(get_current_user)):
 
     if isInviteeEmpty(fr) and not isInviter(user, fr):
         fr_ref.update(
-            {"invitee": user.uid, "status": RequestStatus.WAITING_INVITER.value}
+            {"invitee": user["sub"], "status": RequestStatus.WAITING_INVITER.value}
         )
         sendNotificationToInviter(fr.inviter, user)
         return {"status": "OK"}
@@ -95,7 +98,7 @@ async def makeFriend(inviteeId: str, inviterId: str):
 
 @router.delete("/{RequestId}")
 def deleteRequest(RequestId: str, user=Depends(get_current_user)):
-    if user.uid is None:
+    if user["sub"] is None:
         raise HTTPException(status_code=400, detail="User not found")
 
     Request_ref = db.collection(COLLECTION_NAME).document(RequestId)
@@ -116,7 +119,7 @@ def isRequestExpired(request):
 
 
 def isInviter(user, Request):
-    return Request.inviter == user.uid
+    return Request.inviter == user["sub"]
 
 
 def isRequestDone(Request):
